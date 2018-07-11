fac589ecf5dfde1327524f2aa2d44764561f7390
manjusakajin <sakurano96@gmail.com>
2018-07-11 13:06:18 +0700

    login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  before_action :find_user, only: :create
+
+  def new; end
+
+  def create
+    unless @user&.authenticate(params[:session][:password])
+      flash.now[:danger] = t "error.login"
+      render :new
+      return
+    end
+    log_in @user
+    params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+    redirect_to @user
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  private
+  def find_user
+    @user = User.find_by email: params[:session][:email].downcase
+    flash.now[:danger] = t "error.find_user" unless @user
+    render :new unless @user
+  end
+end
